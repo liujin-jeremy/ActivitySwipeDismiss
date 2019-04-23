fa49fb5796307421f656b52657d6b42881eba49d
liujin <wuxiongc@163.com>
2019-04-23 17:27:09 +0800

    rename package

--- app/src/main/java/teck/threekilogram/dragbacktest/Main2Activity.java
+++ app/src/main/java/teck/threekilogram/dragbacktest/Main2Activity.java
@@ -2,29 +2,18 @@ package teck.threekilogram.dragbacktest;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.support.annotation.NonNull;
-import android.support.v4.widget.ViewDragHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.view.Window;
-import teck.threekilogram.swipeback.SwipeDismiss;
+import teck.liujin.swipeback.SwipeDismiss;
 
 /**
  * @author liujin
  */
 public class Main2Activity extends AppCompatActivity {
 
-      private static final String TAG = "Main2Activity";
-      private SwipeDismiss mSwipeDismiss;
+      private static final String       TAG = "Main2Activity";
+      private              SwipeDismiss mSwipeDismiss;
 
       public static void start (Context context) {
 
--- app/src/main/java/teck/threekilogram/dragbacktest/MainActivity.java
+++ app/src/main/java/teck/threekilogram/dragbacktest/MainActivity.java
@@ -2,27 +2,27 @@ package teck.threekilogram.dragbacktest;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-      public static void start (Context context) {
+      public static void start ( Context context ) {
 
-            Intent starter = new Intent(context, MainActivity.class);
-            context.startActivity(starter);
+            Intent starter = new Intent( context, MainActivity.class );
+            context.startActivity( starter );
       }
 
       @Override
-      protected void onCreate (Bundle savedInstanceState) {
+      protected void onCreate ( Bundle savedInstanceState ) {
 
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            super.onCreate( savedInstanceState );
+            setContentView( R.layout.activity_main );
       }
 
-      public void skipToMain2 (View view) {
+      public void skipToMain2 ( View view ) {
 
-            Main2Activity.start(this);
+            Main2Activity.start( this );
       }
 }
